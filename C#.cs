80a99c90fd7af8d548a70ba73756345a58ea167e
Elijah Kariuki <102251503+Elijah-Kariuki@users.noreply.github.com>
2022-04-06 08:49:56 -0400

    Create C#.cs

---
+++ C#.cs
@@ -0,0 +1,22 @@
+C#
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //This code will make a ghost like creature similar to the Pac-Man game
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(O O)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("The name of the Ghost is Kimutu");
+      
+
+
+     }
+  }
+}
